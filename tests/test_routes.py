e4826102ad0c29dbbc2715e3745a5c7cafb46ef0
Tarek0mran00 <tarek.omra2000@gmail.com>
2026-01-04 04:48:46 -0500

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -136,6 +136,14 @@ class TestAccountService(TestCase):
         # The response should be HTTP 415 UNSUPPORTED_MEDIA_TYPE
         self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
+
+    def test_cors_security(self):
+    """It should return a CORS header"""
+    response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    # Check for the CORS header
+    self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+
     def test_security_headers(self):
     """It should return security headers"""
     response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
